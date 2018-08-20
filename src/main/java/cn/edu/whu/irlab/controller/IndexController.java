cd389ff5e5fe9657e327d5409f773832ba085239
fangrongfu <251591587@qq.com>
2018-08-20 13:27:41 +0800

    添加跳转其它页面的路径

--- src/main/java/cn/edu/whu/irlab/controller/IndexController.java
+++ src/main/java/cn/edu/whu/irlab/controller/IndexController.java
@@ -16,4 +16,9 @@ public class IndexController {
     public String index(){
         return "index";
     }
+
+    @RequestMapping(value = "/lisongfan")
+    public String lisongfan(){
+        return "lisongfan";
+    }
 }
